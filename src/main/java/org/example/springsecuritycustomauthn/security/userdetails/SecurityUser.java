5bb4955500b740ded95b4310066985c64dba28c3
Vitalii Verzun <vitaliy.vz.main@gmail.com>
2026-06-03 21:28:11 +0300

    feat: override isAccountNonLocked(), isEnabled() methods in SecurityUser

--- src/main/java/org/example/springsecuritycustomauthn/security/userdetails/SecurityUser.java
+++ src/main/java/org/example/springsecuritycustomauthn/security/userdetails/SecurityUser.java
@@ -1,18 +1,24 @@
 package org.example.springsecuritycustomauthn.security.userdetails;
 
 import org.example.springsecuritycustomauthn.model.entity.User;
+import org.example.springsecuritycustomauthn.model.enums.UserStatus;
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.util.Assert;
 
 import java.util.Collection;
 
 public class SecurityUser implements UserDetails {
 
-    private User user;
+    private static final String USER_NOT_NULL = "User must not be null";
+
+    private final User user;
 
     public SecurityUser(User user) {
+        Assert.notNull(user, USER_NOT_NULL);
+
         this.user = user;
     }
 
@@ -32,5 +38,15 @@ public class SecurityUser implements UserDetails {
     @Override
     public String getUsername() {
         return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return user.getStatus() != UserStatus.LOCKED;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.getStatus() != UserStatus.DISABLED;
     }
 }
